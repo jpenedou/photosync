bab214756e45f6255c34b7c552d377cfb70f9138
Jose Antonio Penedo <japenedo@gmail.com>
2023-11-17 12:27:41 +0100

    Se crea el proyecto

---
+++ settings.py
@@ -0,0 +1,5 @@
+SOURCE_PATHS = '/mnt/d/movil/debora','/mnt/d/movil/jose'
+#TARGET_PATH = '/mnt/d/fotos'
+TARGET_PATH = '/mnt/d/fotos' # No puede ser una ruta relativa
+TAGNAME_NOTFOUND_PATH = '/mnt/d/fotos/SIN_FECHA' 
+LAST_SYNC_TIME_PATH = '~/photosync/.photosync_last.json'
